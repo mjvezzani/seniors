a7dbfe0fce3d18bd702288bc81abb305642f1474
Michael Vezzani <mjvezzani@gmail.com>
2013-11-16 21:55:21 -0500

    Expanded functionality to include Juniors, Sophomores, and Freshmen. Also added more names to the 'all_students' array.

--- student.rb
+++ student.rb
@@ -11,6 +11,18 @@ class Student
 		grade == 12
 	end
 
+	def junior?
+		grade == 11
+	end
+
+	def sophomore?
+		grade == 10
+	end
+
+	def freshman?
+		grade == 9
+	end
+
 	def to_s
 		"#{last_name}, #{first_name}"
 	end
@@ -20,11 +32,47 @@ def seniors(students)
 	students.select { |student| student.senior? }
 end
 
+def juniors(students)
+	students.select { |student| student.junior? }
+end
+
+def sophomores(students)
+	students.select { |student| student.sophomore? }
+end
+
+def freshmen(students)
+	students.select { |student| student.freshman? }
+end
+
 fred = Student.new("Fred", "James", 12)
 sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
-all_students = [fred, sarah, jack]
+mike = Student.new("Mike", "Johnson", 9)
+allison = Student.new("Allison", "Greenly", 10)
+portia = Student.new("Portia", "McNeary", 9)
+allen = Student.new("Allen", "Davidson", 12)
+all_students = [fred, sarah, jack, mike, allison, portia, allen]
+
+puts "Seniors:"
 
 seniors(all_students).each do |student|
-	puts student
+	puts "* #{student}"
+end
+
+puts "Juniors:"
+
+juniors(all_students).each do |student|
+	puts "* #{student}"
+end
+
+puts "Sophomores:"
+
+sophomores(all_students).each do |student|
+	puts "* #{student}"
+end
+
+puts "Freshmen:"
+
+freshmen(all_students).each do |student|
+	puts "* #{student}"
 end
